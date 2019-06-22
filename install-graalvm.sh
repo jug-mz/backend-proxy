3497bff9a7906239520b0b7c8582cbd8e302e28f
Tim Zöller <tim.zoeller@ilume.de>
2019-06-22 11:48:40 +0200

    Cleaned up Travis file, try and improve cahce

---
+++ install-graalvm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ ! -d "/opt/graalvm-ce-1.0.0-rc16" ]; then 
+	wget https://github.com/oracle/graal/releases/download/vm-1.0.0-rc16/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz -O /opt/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz
+	tar -xzf /opt/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz -C /opt
+fi
