0809a85a16df6e33a1670666fa975cc92f61947c
Tim Zöller <mail@tim-zoeller.de>
2019-06-24 09:40:47 +0200

    Update install-graalvm.sh

--- install-graalvm.sh
+++ install-graalvm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -d "/opt/graalvm-ce-1.0.0-rc16/bin/native-image" ]; then 
+if [ ! -f "/opt/graalvm-ce-1.0.0-rc16/bin/native-image" ]; then 
 	wget https://github.com/oracle/graal/releases/download/vm-1.0.0-rc16/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz -O /opt/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz
 	tar -xzf /opt/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz -C /opt
 fi
