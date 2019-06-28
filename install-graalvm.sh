22d25245e45a2b68a4b4bc3602a6a79e2ce8b522
Tim Zöller <tim.zoeller@ilume.de>
2019-06-28 09:11:18 +0200

    Updated to Quarkus 0.18.0 and GraalVM CE 19.0.0.2

--- install-graalvm.sh
+++ install-graalvm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ ! -d "/opt/graalvm-ce-1.0.0-rc16" ]; then 
-	wget https://github.com/oracle/graal/releases/download/vm-1.0.0-rc16/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz -O /opt/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz
-	tar -xzf /opt/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz -C /opt
+if [ ! -f "/opt/graalvm-ce-19.0.2/bin/java" ]; then
+	wget https://github.com/oracle/graal/releases/download/vm-19.0.2/graalvm-ce-linux-amd64-19.0.2.tar.gz -O /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz
+	tar -xzf /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz -C /opt
+	rm /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz
 fi
