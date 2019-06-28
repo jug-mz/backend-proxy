018a7ee462f4652644985157ea46f9125c93a95d
Tim Zöller <tim.zoeller@ilume.de>
2019-06-28 09:17:17 +0200

    Add native image to GraalVM installation

--- install-graalvm.sh
+++ install-graalvm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ ! -f "/opt/graalvm-ce-19.0.2/bin/java" ]; then
+if [ ! -f "$GRAALVM_HOME/bin/java" ]; then
 	wget https://github.com/oracle/graal/releases/download/vm-19.0.2/graalvm-ce-linux-amd64-19.0.2.tar.gz -O /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz
 	tar -xzf /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz -C /opt
 	rm /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz
+	$GRAALVM_HOME/bin/gu install native-image
 fi
