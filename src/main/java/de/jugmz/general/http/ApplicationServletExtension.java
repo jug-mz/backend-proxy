b2c3e8ad47edbe0b4aac3cc8d9762ebbcf7513fe
Tim Zöller <tim.zoeller@ilume.de>
2019-07-18 22:38:12 +0200

    Reverted https change

--- src/main/java/de/jugmz/general/http/ApplicationServletExtension.java
+++ src/main/java/de/jugmz/general/http/ApplicationServletExtension.java
@@ -10,15 +10,11 @@ import javax.servlet.ServletContext;
 public class ApplicationServletExtension implements ServletExtension {
 
     private final static String HEADER_FILTER_NAME = "header-filter";
-    private final static String HTTPS_FORWARD_FILTER_NAME = "https-filter";
 
     @Override
     public void handleDeployment(final DeploymentInfo deploymentInfo, final ServletContext servletContext) {
         deploymentInfo.addFilter(new FilterInfo(HEADER_FILTER_NAME, CacheHeaderFilter.class));
         deploymentInfo.addFilterUrlMapping(HEADER_FILTER_NAME, "/*", DispatcherType.REQUEST);
-
-        deploymentInfo.addFilter(new FilterInfo(HTTPS_FORWARD_FILTER_NAME, HttpsForwardHeaderFilter.class));
-        deploymentInfo.addFilterUrlMapping(HTTPS_FORWARD_FILTER_NAME, "/*", DispatcherType.REQUEST);
     }
 
 }
