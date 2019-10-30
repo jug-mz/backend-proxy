837eecda4ab7c7d1c024dbd7c4bd6b09211a42ab
Tim Zöller <mail@tim-zoeller.de>
2019-10-30 09:36:55 +0100

    #9: Bumped to Quarkus 0.26.1 and GraalVM 19.2.1 (#16)

--- install-graalvm.sh
+++ install-graalvm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -f "$GRAALVM_HOME/bin/java" ]; then
-	wget https://github.com/oracle/graal/releases/download/vm-19.0.2/graalvm-ce-linux-amd64-19.0.2.tar.gz -O /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz
-	tar -xzf /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz -C /opt
-	rm /opt/graalvm-ce-linux-amd64-19.0.2.tar.gz
+	wget https://github.com/oracle/graal/releases/download/vm-19.2.1/graalvm-ce-linux-amd64-19.2.1.tar.gz -O /opt/graalvm-ce-linux-amd64-19.2.1.tar.gz
+	tar -xzf /opt/graalvm-ce-linux-amd64-19.2.1.tar.gz -C /opt
+	rm /opt/graalvm-ce-linux-amd64-19.2.1.tar.gz
 	$GRAALVM_HOME/bin/gu install native-image
 fi
