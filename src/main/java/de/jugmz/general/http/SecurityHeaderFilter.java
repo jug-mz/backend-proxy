a65ef9af77f0c87ea5a5439946a8916ee9e80a6b
shiftyseaotter <mail@mikhail-golubev.de>
2019-10-31 17:21:50 +0100

    #19 Replaced servlet filter with vertx (#20)

--- src/main/java/de/jugmz/general/http/SecurityHeaderFilter.java
+++ src/main/java/de/jugmz/general/http/SecurityHeaderFilter.java
@@ -1,28 +1,22 @@
 package de.jugmz.general.http;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
-import java.io.IOException;
+import io.quarkus.vertx.http.runtime.filters.Filters;
 
-/**
- * Collection of security headers
- */
-public class SecurityHeaderFilter implements Filter {
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponseWrapper wrapper = new HttpServletResponseWrapper((HttpServletResponse) servletResponse);
-        wrapper.setHeader("Strict-Transport-Security", "max-age=63072000");
-        wrapper.setHeader("Content-Security-Policy", "default-src 'self'; font-src data: 'self'; img-src data: 'self'; frame-ancestors 'none'");
-        wrapper.setHeader("X-Content-Type-Options", "nosniff");
-        wrapper.setHeader("X-Frame-Options", "deny");
-        wrapper.setHeader("X-XSS-Protection", "1; mode=block");
-        filterChain.doFilter(servletRequest, wrapper);
+@ApplicationScoped
+public class SecurityHeaderFilter {
+
+    public void registerMyFilter(@Observes Filters filters) {
+        filters.register(rc -> {
+            rc.response().putHeader("Strict-Transport-Security", "max-age=63072000");
+            rc.response().putHeader("Content-Security-Policy", "default-src 'self'; font-src data: 'self'; img-src data: 'self'; frame-ancestors 'none'");
+            rc.response().putHeader("X-Content-Type-Options", "nosniff");
+            rc.response().putHeader("X-Frame-Options", "deny");
+            rc.response().putHeader("X-XSS-Protection", "1; mode=block");
+
+            rc.next();
+        }, 100);
     }
-
 }
