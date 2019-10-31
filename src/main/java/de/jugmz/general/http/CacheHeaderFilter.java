c982e7bd0225992cbe387202b876eebb33487f55
Mikhail Golubev <mgolubev@Macotter.local>
2019-10-31 16:39:09 +0100

    #19 Replaced servlet filter with vertx

--- src/main/java/de/jugmz/general/http/CacheHeaderFilter.java
+++ src/main/java/de/jugmz/general/http/CacheHeaderFilter.java
@@ -1,15 +1,9 @@
 package de.jugmz.general.http;
 
-import io.undertow.servlet.spec.HttpServletRequestImpl;
+import io.quarkus.vertx.http.runtime.filters.Filters;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
-import java.io.IOException;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
@@ -18,40 +12,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * Static resources should send a Cache Header to the browser
- */
-public class CacheHeaderFilter implements Filter {
+@ApplicationScoped
+public class CacheHeaderFilter {
 
     private final static String EXPIRES_HEADER = "Expires";
-    private final List<String> longCachableContent;
-    private final List<String> mediumCachableContent;
+    private final List<String> longCacheableContent;
+    private final List<String> mediumCacheableContent;
 
     public CacheHeaderFilter() {
-        this.longCachableContent = Stream.of("png", "jpg", "svg").collect(Collectors.toList());
-        this.mediumCachableContent = Stream.of("js", "css").collect(Collectors.toList());
+        this.longCacheableContent = Stream.of("png", "jpg", "svg").collect(Collectors.toList());
+        this.mediumCacheableContent = Stream.of("js", "css").collect(Collectors.toList());
     }
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-        if (longCachableContent.stream().anyMatch(fileExtension -> ((HttpServletRequestImpl) servletRequest).getServletPath().endsWith(fileExtension))) {
-            HttpServletResponseWrapper wrapper = new HttpServletResponseWrapper((HttpServletResponse) servletResponse);
-            wrapper.setHeader(EXPIRES_HEADER, buildDateStringPlusDays(365));
-            filterChain.doFilter(servletRequest, wrapper);
-        } else if (mediumCachableContent.stream().anyMatch(fileExtension -> ((HttpServletRequestImpl) servletRequest).getServletPath().endsWith(fileExtension))) {
-            HttpServletResponseWrapper wrapper = new HttpServletResponseWrapper((HttpServletResponse) servletResponse);
-            wrapper.setHeader(EXPIRES_HEADER, buildDateStringPlusDays(30));
-            filterChain.doFilter(servletRequest, wrapper);
-        } else {
-            filterChain.doFilter(servletRequest, servletResponse);
-        }
-
+    public void registerMyFilter(@Observes Filters filters) {
+        filters.register(rc -> {
+            if (longCacheableContent.stream().anyMatch(fileExtension -> rc.normalisedPath().endsWith(fileExtension))) {
+                rc.response().putHeader(EXPIRES_HEADER, buildDateStringPlusDays(365));
+            } else if (mediumCacheableContent.stream().anyMatch(fileExtension -> rc.normalisedPath().endsWith(fileExtension))) {
+                rc.response().putHeader(EXPIRES_HEADER, buildDateStringPlusDays(30));
+            }
+            rc.next();
+        }, 101);
     }
 
     private String buildDateStringPlusDays(int days) {
         OffsetDateTime oneHourFromNow = OffsetDateTime.now(ZoneOffset.UTC).plus(Duration.ofDays(days));
         return DateTimeFormatter.RFC_1123_DATE_TIME.format(oneHourFromNow);
     }
-
 }
